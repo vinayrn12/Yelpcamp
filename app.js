7867f005f33ba663daf69cdba3469518ffa5f6c7
Vinay <vinayvrn12@gmail.com>
2020-04-17 19:42:22 +0530

    app launch

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var campgroundRoutes        = require("./routes/campgrounds"),
     indexRoutes             = require("./routes/index");
 
 // mongoose.connect("mongodb://localhost:27017/yelp_camp_final", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-mongoose.connect("mongodb+srv://vinay:%2ASANJANArn12@cluster0-nwvpl.mongodb.net/test", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect("mongodb+srv://vinay:%2ASANJANArn12@yelp-7egas.azure.mongodb.net/test", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
